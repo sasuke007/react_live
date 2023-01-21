37395f3ffbe11beafaeb8b90221990d16f664ffe
= <pandit.rohit0007@gmail.com>
2023-01-21 12:04:24 +0530

    react live

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * HMR - Hot module reloading
+ * 
+ * file watcher algorithm -> written in c++
+ * 
+ * 
+ */
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
+
+const heading = React.createElement("h1",
+    {
+        id: "title"
+    },
+    "Heading 2");
+
