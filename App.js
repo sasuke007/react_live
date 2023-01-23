302b27f95688cd2f07e86fe53647a6c8ca464f46
= <pandit.rohit0007@gmail.com>
2023-01-23 13:46:13 +0530

    food villa

--- App.js
+++ App.js
@@ -6,13 +6,53 @@
  * 
  */
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement("h1",
-    {
-        id: "title"
-    },
-    "Heading 2"
+const Title = () => (
+    <a href="/">
+        <img src="/Users/rohitpandit/react_live/food_villa.jpg" alt="logo" className="logo" />
+    </a>
 );
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Title />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <h4> Body </h4>
+    );
+}
+
+const Footer = () => {
+    (
+        <h4>Footer</h4>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer/>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
 
